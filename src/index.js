5713c152adc6dbf468e606169034fac7cb0eb56a
Pavel <73703906+meow-or@users.noreply.github.com>
2022-07-28 02:11:12 +0500

    minimize css, html, js; setup dev server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+import * as $ from 'jquery'
 import Post from './Post'
 import json from './assets/json.json'
+import xml from './assets/data.xml'
+import Wlogo from './assets/logo.png'
 import './styles/styles.css'
 
-const post = new Post('Webpack Post Title')
+const post = new Post('Webpack Post Title', Wlogo)
+
+$('pre').addClass('code').html(post.toString())
 
 console.log('Post to String:', post.toString())
 
 console.log('JSON:', json)
+console.log('XML:', xml)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,31 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+
+  if(isProd) {
+    config.minimizer = [
+      new CssMinimizerWebpackPlugin(),
+      new TerserWebpackPlugin()
+    ]
+  }
+
+  return config
+}
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+console.log('is dev:', isDev)
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -13,18 +38,51 @@ module.exports = {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
+  resolve: {
+    extensions: ['.js', '.json', '.css', '.png', '.jpg', '.xml'],
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4200
+  },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './index.html'
+      template: path.resolve(__dirname, './src/index.html'),
+      minify: {
+        collapseWhitespace: isProd
+      }
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/icons8.ico'),
+          to: path.resolve(__dirname, 'dist')
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
   ],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader']
+      },
+        {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      },
     ]
   }
 }
